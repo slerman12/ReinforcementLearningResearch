dd359b4dd5b10bdef0e67ad6ba3823736155fb4b
slerman12 <slerman@u.rochester.edu>
2018-05-16 18:32:30 -0400

    ms pacman

--- Run_MsPacman_Rand_Proj.py
+++ Run_MsPacman_Rand_Proj.py
@@ -150,7 +150,7 @@ agent = Agent(vision=vision, long_term_memory=long_term_memory, short_term_memor
               attributes=attributes, actions=action_space, epsilon=1, k=50)
 
 # File name
-filename_prefix = "Segmentation_no_resize_more_episodes"
+filename_prefix = "Rand_Proj_no_resize_more_episodes"
 filename = "{}_{}_{}___{}".format(filename_prefix, env_name, datetime.datetime.today().strftime('%m_%d_%y'),
                                   datetime.datetime.now().strftime('%H_%M'))
 
